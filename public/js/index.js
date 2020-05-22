5031406792478a0b318b666293ee535cf61674af
Esteban Camargo <estebmaister@gmail.com>
2020-05-22 02:51:35 -0400

    :lipstick: Adding animated cat

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ document.onmousemove = function () {
   var x = (event.clientX * 8) / window.innerWidth + "%";
   var y = (event.clientY * 8) / window.innerHeight + "%";
 
-  for (var i = 0; i < 2; i++) {
+  for (var i = 0; i < 4; i++) {
     pupil[i].style.left = x;
     pupil[i].style.top = y;
     pupil[i].style.transform = "translate(" + x + "," + y + ")";
